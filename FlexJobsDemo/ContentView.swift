4c4a234ed40a67d1279da2d3f0f9a8e3be0702a8
rick2785 <rjhrabowskie@gmail.com>
2020-03-30 14:33:29 -0400

    Initial Commit

---
+++ FlexJobsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlexJobsDemo
+//
+//  Created by RJ Hrabowskie on 3/30/20.
+//  Copyright © 2020 RJ Hrabowskie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
